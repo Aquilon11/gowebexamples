316db1b6ad470ce477c87b04b19de594e0e9d80c
Aquilon11 <aquilon11@gmail.com>
2021-06-26 19:50:17 +0200

    HTTP Server

--- main.go
+++ main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
-    })
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Welcome to my website!")
+	})
 
-    http.ListenAndServe(":80", nil)
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.ListenAndServe(":80", nil)
 }
-
